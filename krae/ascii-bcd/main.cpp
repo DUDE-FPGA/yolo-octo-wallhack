c7dde2e045e4b1b0167003eed3b791e9f86d2af0
Vladimir Shulyak <astraeus@shulyak.co.uk>
2014-02-26 11:54:08 +0000

    Initial ascii to bcd conversion

--- krae/ascii-bcd/main.cpp
+++ krae/ascii-bcd/main.cpp
@@ -1,12 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int ascii_to_bcd(char c){
+    switch(c){
+        case 0b110001: return 0b0001;
+        case 0b110010: return 0b0010;
+        case 0b110011: return 0b0011;
+        case 0b110100: return 0b0100;
+        case 0b110101: return 0b0101;
+        case 0b110110: return 0b0110;
+        case 0b110111: return 0b0111;
+        case 0b111000: return 0b1000;
+        case 0b111001: return 0b1001;
+    }
+}
 
 int main()
 {
-	int input_ascii;
-	int output_bcd = 0b1010;
-	//output_bcd = ~output_bcd;
-	printf("%d\n",output_bcd);
-	printf("%d\n",output_bcd & 0b0000);
+	string input_ascii;
+    int i = 0;
+    cout << "Input ascii" << endl;
+    cin >> input_ascii;
+    cout << "You input " << input_ascii << endl;
+
+    for(char& c : input_ascii){
+        cout << ascii_to_bcd(c) + 10 << " ";
+    }
+    cout << endl;
+
 	return 0;
 }
